9892aa9dc43bab8b32ce2352ffe5177f3e8dfb10
Shawn Michael <blkmajik@monkeyspunk.net>
2022-06-17 09:53:47 -0600

    Fix header references for CRACK vs CRACKLIB

--- php_cracklib.h
+++ php_cracklib.h
@@ -1,17 +1,17 @@
 /* crack extension for PHP */
 
-#ifndef PHP_CRACK_H
-# define PHP_CRACK_H
+#ifndef PHP_CRACKLIB_H
+# define PHP_CRACKLIB_H
 #include <crack.h>
 
 extern zend_module_entry crack_module_entry;
 # define phpext_crack_ptr &crack_module_entry
 
-# define PHP_CRACK_VERSION "1.0"
+# define PHP_CRACKLIB_VERSION "1.0"
 
-# if defined(ZTS) && defined(COMPILE_DL_CRACK)
+# if defined(ZTS) && defined(COMPILE_DL_CRACKLIB)
 ZEND_TSRMLS_CACHE_EXTERN()
 # endif
 
-#endif	/* PHP_CRACK_H */
+#endif	/* PHP_CRACKLIB_H */
 
